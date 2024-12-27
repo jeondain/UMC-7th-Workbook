71c2152882c83d15ce0d54e1974941264c485dff
jeondain <dain.jeon03@gmail.com>
2024-12-28 01:48:32 +0900

    [FEAT #17] Spring Security를 활용한 로그인 및 회원가입 구현

--- src/main/java/umc7/spring/domain/Member.java
+++ src/main/java/umc7/spring/domain/Member.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import umc7.spring.domain.common.BaseEntity;
 import umc7.spring.domain.enums.Gender;
 import umc7.spring.domain.enums.MemberStatus;
+import umc7.spring.domain.enums.Role;
 import umc7.spring.domain.enums.SocialType;
 import umc7.spring.domain.mapping.MemberAgree;
 import umc7.spring.domain.mapping.MemberMission;
@@ -52,8 +53,14 @@ public class Member extends BaseEntity {
 
     private LocalDate inactiveDate;
 
-    //    @Column(nullable = false, length = 50)
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @ColumnDefault("0")
     private Integer point;
@@ -69,4 +76,8 @@ public class Member extends BaseEntity {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<MemberMission> memberMissionList = new ArrayList<>();
+
+    public void encodePassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/main/java/umc7/spring/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package umc7.spring.domain.enums;
+
+public enum Role {
+    ADMIN, USER
+}
